d4c077edb4780613217fa0a8b0ca5575bc1e7e2e
sing3demons <sing3demons@live.com>
2025-09-21 13:38:41 +0700

    feat: Remove commented-out login endpoint from OAuth routes

--- main.go
+++ main.go
@@ -177,7 +177,6 @@ func setupFullRouter(cfg *config.Config, authHandler *handlers.AuthHandler, oaut
 			{
 				oauth.GET("/authorize", oauthHandler.Authorize)
 				oauth.POST("/authorize", oauthHandler.Authorize)
-				// /api/auth/oauth/login
 				oauth.POST("/login", oauthHandler.LoginAndAuthorize)
 				oauth.POST("/token", oauthHandler.Token)
 				oauth.GET("/userinfo", oauthHandler.UserInfo)
